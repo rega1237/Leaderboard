d9cd3a47db6b952962382643c5999f11db0606f1
Rafael Guzman <rega1237@gmail.com>
2022-05-20 11:25:50 -0400

    make docs folder as the output

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   devServer: {
     static: {
-      directory: path.resolve(__dirname, 'dist'),
+      directory: path.resolve(__dirname, 'docs'),
     },
     port: 3000,
     open: true,
@@ -20,7 +20,7 @@ module.exports = {
     }),
   ],
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js',
   },
   module: {
